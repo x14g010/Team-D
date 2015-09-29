0dd13c3b22b674538fc495ef38bfdf904ed711ba
x14g021 <x14g021@PC504N026.chiba-fjb.ac.jp>
2015-09-29 11:36:29 +0900

    更新

--- src/tameD/system.java
+++ src/tameD/system.java
@@ -62,15 +62,18 @@ public class system extends HttpServlet {
 
 			//テーブルが無ければ作成
 			if(!mOracle.isTable("com")){
-				mOracle.execute("create sequence seq");
+				mOracle.execute("create sequence seq1");
 				mOracle.execute("create table com(comID number,usNAME varchar2(50),usID number"
 								+ ",comDATE DATE,comMSG varchar(200))");
 			}
-			if(!mOracle.isTable("genre"))
+			if(!mOracle.isTable("genre")){
+				mOracle.execute("create sequence seq2");
 				mOracle.execute("create table genre(genID number,genNAME varchar2(50))");
-			if(!mOracle.isTable("kiji"))
+				}
+			if(!mOracle.isTable("kiji")){
+				mOracle.execute("create sequence seq3");
 				mOracle.execute("create table kiji(kijiID number,kijiTITLE varchar2(100),kijiMSG varchar(200),"
-								+ "kijiDATE DATE)");
+								+ "kijiDATE DATE)");}
 
 			} catch (Exception e) {
 			System.err.println("db.txtにユーザ情報が設定されていない、もしくは認証に失敗しました");
@@ -122,6 +125,7 @@ public class system extends HttpServlet {
         try {
 			//ジャンル読み込み
         	ResultSet gen = mOracle.query("select * from genre");
+	        String param1 = request.getParameter("j");
 			while(gen.next())
 			{
 				int id =gen.getInt(1);
@@ -135,20 +139,8 @@ public class system extends HttpServlet {
 				}
 			}
 			//メッセージの置換
-	        ts.replace("$(GENRE)", sb.toString());
-	        //ジャンル選択時、ページの読み込み
-	        Keijiban p1 = new Keijiban();
-	        p1.open(this, "kijise.html");
-	      //パラメータによって内容を切り替え
-	        String param1 = request.getParameter("j");
-	        if (param1 != null && param1.length() > 0)
-	        {
-	        	int index =  Integer.parseInt(param1);
-	        	if(index == 1)
-	        		ts.replace("$(PAGE)", p1.getText());
-	        }
-	        else{
-        }
+	        ts.replace("$(GENRE)", sb.toString());;
+
 
 
 	        //ジャンル記事読み込み
@@ -174,15 +166,42 @@ public class system extends HttpServlet {
 			//メッセージの置換
 	        ts.replace("$(PAGE)", sb.toString());
 
+	        //記事内容
+	        //データの抽出
+	        try {
+				ResultSet res = mOracle.query("select * from com");
+				while(res.next())
+				{
+					String data = res.getString(5);
+					//日付の受け取り
+					Calendar cal = Calendar.getInstance();
+					cal.setTime(res.getDate(4));
+					if(data != null)
+					{
+						//文字列バッファにメッセージ内容を貯める
+						//CONVERTはタグの無効化
+						sb.append(String.format("<hr>%d:%s:%d:%d年%d月%d日 %d時%d分<br>%s<BR>\n",
+												cal.get(Calendar.YEAR),
+												cal.get(Calendar.MONTH)+1,
+												cal.get(Calendar.DAY_OF_MONTH),
+												cal.get(Calendar.HOUR_OF_DAY),
+												cal.get(Calendar.MINUTE),
+												 CONVERT(data)));
+					}
+				}
+				//メッセージの置換
+		        ts.replace("$(KIJI)", sb.toString());
+			} catch (SQLException e) {}
+
 	        //記事コメント
-	      //パラメータにデータがあった場合はDBへ挿入
+	        //パラメータにデータがあった場合はDBへ挿入
 	        String param2 = request.getParameter("data1");
-	        if (param1 != null && param1.length() > 0)
+	        if (param2 != null && param2.length() > 0)
 	        {
 	        	//UTF8をJava文字列に変換
-	        	String data1 = new String(param1.getBytes("ISO-8859-1"),"UTF-8");
+	        	String data1 = new String(param2.getBytes("ISO-8859-1"),"UTF-8");
 	        	//SQL文の作成 Oracle.STRはシングルクオートのエスケープ処理
-	        	String sql = String.format("insert into com values(seq.nextval,'%s','%s',sysdate)",Oracle.STR(data1));
+	        	String sql = String.format("insert into com values(seq1.nextval,'%s','%s',sysdate)",Oracle.STR(data1));
 	        	//デバッグ用
 	        	System.out.println("DEBUG:SQL文 "+sql);
 	        	//DBにSQL文を実行させる
@@ -214,9 +233,6 @@ public class system extends HttpServlet {
 		        ts.replace("$(COM)", sb.toString());
 			} catch (SQLException e) {}
 
-	      //選択時、ページの読み込み
-	        Keijiban k1 = new Keijiban();
-	        k1.open(this, "kiji.html");
 
 	        //管理者画面へ
 
